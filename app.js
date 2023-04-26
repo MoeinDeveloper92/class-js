1dc4fddd6d6c2c50d9fd39bd4e87a5de7f605378
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-04-26 13:15:50 +0430

    another version

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ class Car {
         this.engine = engine
         this.color = color
     }
+
+    show(newCar) {
+        for (let key in newCar) {
+            console.log(`${key}:${newCar[key]}`)
+        }
+    }
 }
 
 const newCar = new Car(4, 'V6', 'Gray')
+
+newCar.show(newCar)
 // bellow we show how to make use of object literal
-for (let key in newCar) {
-    console.log(`${key}:${newCar[key]}`)
-}
+// we have created another instance of Class
+
 
